5f730d9e11d2b65be37ba65db4866849fbb2e6b3
Aurotis <117131109+Aurotis@users.noreply.github.com>
2022-11-04 03:11:26 -0700

    Create Course.java

---
+++ team-01-03-ver0.5/src/application/personalIndexCardManager/Course.java
@@ -0,0 +1,14 @@
+package application.personalIndexCardManager;
+
+import java.util.List;
+
+// temp
+public class Course {
+
+	private String name;
+	private List<String> indexCards;
+	
+	public Course(String name) {
+		this.name = name;
+	}
+}
