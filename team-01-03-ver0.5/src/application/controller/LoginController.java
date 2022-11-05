1589ef9a1c66e807ef789419f918ad710ba9c4c9
Aurotis <117131109+Aurotis@users.noreply.github.com>
2022-11-04 22:24:45 -0700

    Build 2
    - updated LoginController

--- team-01-03-ver0.5/src/application/controller/LoginController.java
+++ team-01-03-ver0.5/src/application/controller/LoginController.java
@@ -4,10 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.personalIndexCardManager.SQLConnector;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -56,8 +59,13 @@ public class LoginController implements Initializable {
 	 * and are valid in the database. Goes to user's courses page.
 	 * 
 	 */
-	@FXML public void userLogin(ActionEvent event) {
-				
+	@FXML public void userLogin(ActionEvent event) throws IOException {
+		
+		// Sets the visibility of the labels to false
+		errorLabel.setVisible(false);
+		alertEmail.setVisible(false);
+		alertPass.setVisible(false);
+		
 		// Checks to see if the email field or password field is empty
 		if (emailField.getText().isEmpty() || passwordField.getText().isEmpty()) {
 			
@@ -70,27 +78,43 @@ public class LoginController implements Initializable {
 				alertEmail.setVisible(true);						
 			if (passwordField.getText().isEmpty()) 
 				alertPass.setVisible(true);
-			
+			return;
 		}
-		else {
-			// Get inputs
-			String enteredEmail = emailField.getText();
-			sop("Email   : " + enteredEmail);
-			String enteredPass = passwordField.getText();
-			sop("Password: " + enteredPass);
-			
-			// Check inputs with database
-			
-			
-			// Sets the visibility of the labels to false
-			errorLabel.setVisible(false);
-			alertEmail.setVisible(false);
-			alertPass.setVisible(false);
-			
-			// Changes the scene from login to user's courses window
-			this.changeScene(event, "fxml/success.fxml");
-			
+		// Get inputs
+		String enteredEmail = emailField.getText();
+		sop("Email   : " + enteredEmail);
+		String enteredPass = passwordField.getText();
+		sop("Password: " + enteredPass);
+				
+		// Check inputs with database 
+		SQLConnector con = new SQLConnector();
+		
+		// Check to see if the email is valid
+		if (!(con.findEmail("usersTest.db", "Users", enteredEmail))) {
+			errorLabel.setText("***The email is invalid***");
+			errorLabel.setVisible(true);
+			alertEmail.setVisible(true);
+			return;
 		}
+		
+		// Check to see if the password is valid
+		// Also checks email again with password for redundancy
+		if(!(con.findPass("usersTest.db", "Users", enteredEmail, enteredPass))) {
+			errorLabel.setText("***The password is invalid***");
+			errorLabel.setVisible(true);
+			alertPass.setVisible(true);
+			return;
+		}
+		
+					
+		// Sets the visibility of the labels to false
+		errorLabel.setVisible(false);
+		alertEmail.setVisible(false);
+		alertPass.setVisible(false);
+					
+		// Changes the scene from login to user's courses window
+		this.changeScene(event, "fxml/success.fxml");
+			
 	}
 
 	/*
